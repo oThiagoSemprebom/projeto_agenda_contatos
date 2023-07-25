5aa1e607382a23d668d574c6ace0efea471b717a
thiago <thiagosemprebom2016@gmail.com>
2023-07-25 16:46:44 -0300

    commit inicial

---
+++ mains.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('form-contato')
+const contatos = [];
+const telefones = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+})
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputTelefoneContato = document.getElementById('telefone-contato');
+
+    contatos.push(inputNomeContato.value);
+    telefones.push(inputTelefoneContato.value);
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputTelefoneContato.value}</td>`;
+    
+    linhas += linha;
+
+    inputNomeContato.value = '';
+    inputTelefoneContato.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
